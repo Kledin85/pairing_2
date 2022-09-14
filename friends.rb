9cecf9c9b80bb697a9ee0f5689e88aa320f4e583
Kyle Ledin <Kyle.Ledin85@gmail.com>
2022-09-14 13:30:28 -0500

    add elsif statement

--- friends.rb
+++ friends.rb
@@ -3,3 +3,7 @@ friends = ["wes", "sam", "jacob"]
 if bestfriend == "John"
   puts "#{bestfriend} is a very good friend"
 end
+if friends[0] == bestfriend
+elsif friends[0] == "wes"
+  puts "#{friends[0]} is also a good friend"
+end
